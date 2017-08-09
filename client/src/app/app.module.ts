1af785d63e00245c8743881d6cea4370371def0a
Adel Acebal <adelacebal@yahoo.com>
2017-08-09 18:30:10 -0400

    removing modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,14 +4,6 @@ import { routes } from './app.routing';
 import { RouterModule } from "@angular/router";
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule, MdIconRegistry } from "@angular/material";
-
-import { VgCoreModule } from 'videogular2/core';
-import { VgControlsModule } from 'videogular2/controls';
-import { VgOverlayPlayModule } from 'videogular2/overlay-play';
-import { VgBufferingModule } from 'videogular2/buffering';
-import { Overlay, overlayConfigFactory } from 'angular2-modal';
-import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -29,14 +21,9 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     HttpModule,
     BrowserModule,
-    MaterialModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule,
     RouterModule.forRoot(routes),
   ],
-  providers: [MdIconRegistry],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/landing-page/landing-page.component.ts
+++ client/src/app/landing-page/landing-page.component.ts
@@ -1,25 +1,19 @@
 import { Component, OnInit, ViewChild, Injectable } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { SignupComponent } from '../signup/signup.component';
-import { Overlay, overlayConfigFactory } from 'angular2-modal';
-import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
-  providers: [Modal]
 })
 export class LandingPageComponent implements OnInit {
 
   popup: SignupComponent;
 
-  constructor(public modal: Modal) { }
+  constructor() { }
 
   ngOnInit() {
-  }
- openCustom() {
-    return this.modal.open(LoginComponent, BSModalContext);
   }
 
 }
